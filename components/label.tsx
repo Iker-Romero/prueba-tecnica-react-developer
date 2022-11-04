e92d6aaab0ccf8013c2f325a145ef913070441bc
Iker-Romero <ikersoftdev@gmail.com>
2022-11-04 17:02:03 +0100

    Boilerplate adapted

--- components/label.tsx
+++
@@ -1,7 +0,0 @@
-const Label = (props: { targetID: string; text: string }) => {
-  const { targetID, text } = props;
-
-  return <label htmlFor={targetID}>{text}</label>;
-};
-
-export default Label;
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,8 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import ErrorPage from './error-page';
-import Finder from './routes/finder';
 import Home from './routes/home';
 import Root from './routes/root';
-import Saved from './routes/saved';
-import ShowDetails from './routes/show-details';
-import ShowForm from './routes/show-form';
 
 const router = createBrowserRouter([
   {
@@ -20,22 +16,6 @@ const router = createBrowserRouter([
       {
         path: '',
         element: <Home />,
-      },
-      {
-        path: 'finder',
-        element: <Finder />,
-      },
-      {
-        path: 'show-form',
-        element: <ShowForm />,
-      },
-      {
-        path: 'saved',
-        element: <Saved />,
-      },
-      {
-        path: '/saved/:id',
-        element: <ShowDetails />,
       },
       {
         path: '*',
