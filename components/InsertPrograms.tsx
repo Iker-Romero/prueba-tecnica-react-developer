94ea4dd56ec440872fc0066c9bb30b08512682a7
Iker-Romero <ikersoftdev@gmail.com>
2022-11-06 17:26:10 +0100

    figure click handled and render test working

--- components/InsertPrograms.tsx
+++ components/InsertPrograms.tsx
@@ -6,38 +6,42 @@ import StyledSection from '../ui/StyledSection';
 
 const InsertPrograms = React.memo(function InsertPrograms(props: {
   programs: ProgramType[];
+  handleClick: (program: ProgramType) => void;
 }) {
-  const { programs } = props;
+  const { programs, handleClick } = props;
 
   return (
-    <StyledSection>
-      {programs.map((program, i) => {
-        while (i < 20) {
-          const {
-            title,
-            images: {
-              'Poster Art': { url },
-            },
-          }: ProgramType = program;
+    <>
+      <StyledSection>
+        {programs.map((program, i) => {
+          while (i < 20) {
+            const {
+              title,
+              images: {
+                'Poster Art': { url },
+              },
+            }: ProgramType = program;
 
-          return (
-            <StyledFigure
-              key={title}
-              onClick={() => {
-                // popUpProgram = program;
-                console.log(program);
-                // console.log(popUpProgram);
-                // setPopUpState(true);
-              }}
-              // ref={figureRef}
-            >
-              <img src={url} alt={`${title} poster`} />
-              <figcaption>{title}</figcaption>
-            </StyledFigure>
-          );
-        }
-      })}
-    </StyledSection>
+            return (
+              <StyledFigure
+                key={title}
+                onClick={() => handleClick(program)}
+                //   {
+                //   popUpProgram = program;
+                //   console.log(program);
+                //   console.log(popUpProgram);
+                //   setPopUpState(true);
+                // }}
+                // ref={figureRef}
+              >
+                <img src={url} alt={`${title} poster`} />
+                <figcaption>{title}</figcaption>
+              </StyledFigure>
+            );
+          }
+        })}
+      </StyledSection>
+    </>
   );
 });
 
--- src/routes/ProgramsPage.tsx
+++ src/routes/ProgramsPage.tsx
@@ -25,14 +25,17 @@ interface PosterArtType {
   height: number;
 }
 
+let popUpProgram: ProgramType;
+
 const ProgramsPage = (props: { type: 'series' | 'movies' }) => {
   const { type } = props;
 
   const [programs, setPrograms] = useState([]);
 
-  const [popUpState, setPopUpState] = useState(false);
-  // const [popUpProgram, setPopUpProgram] = useState(false);
-  const figureRef = useRef(null);
+  // const [popUpState, setPopUpState] = useState(false);
+
+  const [popUpProgram, setPopUpProgram] = useState<ProgramType>();
+  // const figureRef = useRef(null);
 
   useEffect(() => {
     (async () => {
@@ -53,44 +56,19 @@ const ProgramsPage = (props: { type: 'series' | 'movies' }) => {
     })();
   }, []);
 
-  let popUpProgram;
-  if (popUpState) {
-    console.log(popUpProgram);
-  }
+  const handleClick = (program: ProgramType) => {
+    console.log('click');
+    if (program) setPopUpProgram(program);
+  };
 
   return (
     <>
       <h1>Popular {type}</h1>
-      {popUpState && console.log(popUpProgram)}
-      {console.log(popUpProgram)}
+      {/* {popUpState && console.log(popUpProgram)}
+      {console.log(popUpProgram)} */}
       {console.log('me renderizo')}
-      <InsertPrograms programs={programs} />
-      {/* {programs.map((program, i) => {
-          while (i < 20) {
-            const {
-              title,
-              images: {
-                'Poster Art': { url },
-              },
-            }: ProgramType = program;
-
-            return (
-              <StyledFigure
-                key={title}
-                onClick={() => {
-                  popUpProgram = program;
-                  console.log(program);
-                  console.log(popUpProgram);
-                  setPopUpState(true);
-                }}
-                ref={figureRef}
-              >
-                <img src={url} alt={`${title} poster`} />
-                <figcaption>{title}</figcaption>
-              </StyledFigure>
-            );
-          }
-        })} */}
+      <InsertPrograms programs={programs} handleClick={handleClick} />
+      {popUpProgram ? <div>HOLA</div> : null}
     </>
   );
 };
